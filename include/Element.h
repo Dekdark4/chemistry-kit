3a4dc22d90610e16aa043c9bb5e16985fe416592
Dekdark4 <zzkskl@gmail.com>
2026-05-18 21:24:38 +0300

    Создал структуру Element с пространством имён Elements. Создал базовые поля структуры, базовый и перегруженный конструкторы, метод вывода элемента на экран.

---
+++ include/Element.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <cstdint>
+#include <string>
+
+namespace Elements {
+
+	struct Element
+	{
+		// ПОЛЯ
+		uint16_t atomic_number{};
+		std::string symbol{};
+		std::string name_en{};
+		std::string name_ru{};
+		double atomic_mass{};
+		uint16_t period{};
+		uint16_t group{};
+
+		// КОНСТРУКТОРЫ
+		Element() = default;
+		Element(uint16_t atomic_number, std::string symbol, std::string name_en, std::string name_ru, double atomic_mass, uint16_t period, uint16_t group) :
+			atomic_number{ atomic_number },
+			symbol{ std::move(symbol) },
+			name_en{ std::move(name_en) },
+			name_ru{ std::move(name_ru) },
+			atomic_mass( atomic_mass ),
+			period(period ),
+			group (group) {};
+
+		// МЕТОДЫ
+		void print();
+	};
+
+}
---
+++ src/Element.cpp
@@ -0,0 +1,13 @@
+﻿#include "Element.h"
+#include <iostream>
+
+void Elements::Element::print()
+{
+	std::cout << "Атомный номер: " << atomic_number << std::endl;
+	std::cout << "Международный символ : " << symbol << std::endl;
+	std::cout << "Название (англ.): " << name_en << std::endl;
+	std::cout << "Название (рус.): " << name_ru << std::endl;
+	std::cout << "Атомная масса: " << atomic_mass << std::endl;
+	std::cout << "Номер периода: " << period << std::endl;
+	std::cout << "Номер группы: " << group << std::endl;
+}
