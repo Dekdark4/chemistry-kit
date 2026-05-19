97ec39ca17e98c91ad4364c3d2b348744f2e5c84
Dekdark4 <zzkskl@gmail.com>
2026-05-19 09:06:20 +0300

    Добавил простейшую реализацию метода find_quoted_text() для определения и извлечения строки из двойных ковычек.

--- include/JsonParser.h
+++ include/JsonParser.h
@@ -1,11 +1,17 @@
 ﻿#pragma once
+#include <string>
+#include <optional>
 
 namespace JsonParser
 {
 	class JsonParser
 	{
 	private:
+		std::string text;
 	public:
 		JsonParser() = default;
+		JsonParser(std::string text) : text{ std::move(text) } {};
+
+		std::optional<std::string> find_quoted_text();
 	};
 }
--- src/JsonParser.cpp
+++ src/JsonParser.cpp
@@ -1,2 +1,36 @@
 ﻿#include "JsonParser.h"
-#include <fstream>
+#include <iostream>
+
+std::optional<std::string> JsonParser::JsonParser::find_quoted_text()
+{
+    // Проверяем, не слишком ли короткая строка
+    if (text.size() < 2)
+    {
+        std::cerr << "Строка слишком короткая" << std::endl;
+        return std::nullopt;
+    }
+
+    // Проверяем, действительно ли строка начинается с "
+    if (text.front() != '"')
+    {
+        std::cerr << "Строка начинается не с кавычки." << std::endl;
+        return std::nullopt;
+    }
+
+    // Проверяем, действительно ли строка заканчивается на "
+    if (text.back() != '"')
+    {
+        std::cerr << "Строка заканчивается не кавычкой." << std::endl;
+        return std::nullopt;
+    }
+
+    // Проходимся по строке и возвращаем строку без кавычек
+    std::string unquoted_text{};
+    for (auto i{ 1 }; i < text.size() - 1; ++i)
+    {
+        unquoted_text += text[i];
+    }
+
+    // Единственно верный исход
+    return unquoted_text;
+}
